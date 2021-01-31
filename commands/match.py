16b8cd1e283fdfbe59097249a47113783597830b
theo-brown <7982453+theo-brown@users.noreply.github.com>
2021-01-31 11:01:24 +0000

    Replace match_message with embed; Add intents.members

--- commands/match.py
+++ commands/match.py
@@ -3,70 +3,52 @@ EVL Discord Bot template by Theo Brown
 Licensed under GNU GPLv3
 https://github.com/theo-brown/EVL-bot-template
 """
-from datetime import datetime
+from discord import Embed
 
 help_message = \
 """
 ```
 !match @Team1 @Team2 [-d DATE -t TIME]
-DATE: Accepted formats: DD/MM, DD/MM, Monday, Tuesday, etc, Mon, Tue, etc
-TIME: Accepted formats: HHMM, HH:MM, HHMM[AM/PM], etc
+DATE: The day or date that the match will be played, provided as a string with no spaces
+TIME: The times that the match will be played, provided as a string with no spaces
 """
 
 async def run(message, args, kwargs):
-    
-    #date_formats = ["%d/%m", "%d.%m", "%a", "%A", "%a"]
-    #time_formats = ["%H%M","%H:%M", "%I%M", "%I:%M", "%I%M%p", "%I:%M%p", "%I%M %p", "%I:%M %p"]
-    help_message_text = "Usage: `-match <@Team1> <@Team2> [-d date -t time]`"
-    date = 'Today '
-    time = ''
-    
-    if 'help' in kwargs.keys() or 'h' in kwargs.keys():
-        await message.channel.send(help_message_text)
+
+    date = "Today "
+    time = ""
+
+    if len(args) != 2 or len(message.role_mentions) != 2:
+        await message.channel.send("Error: Please @ exactly two teams.")
         return
-    
-    if len(args) != 2:
-        await message.channel.send("Error: Please provide exactly two teams.")
-        return
-    
-    team1 = args[0]
-    team2 = args[1]
-    
+
+    team1, team2 = message.role_mentions
+    team1_players, team2_players = "", ""
+    for user in message.guild.members:
+        if team1 in user.roles:
+            team1_players += user.mention + '\n'
+        if team2 in user.roles:
+            team2_players += user.mention + '\n'    
+
     if 'd' in kwargs.keys():
-        date = kwargs['d']
-        #unmatched_formats = 0
-        #for date_format in date_formats:
-        #    try:
-        #        datetime.strptime(date, date_format)
-        #        break
-        #    except ValueError:
-        #        unmatched_formats +=1
-        #if unmatched_formats == len(date_formats):
-        #    await message.channel.send("Error: Date `{}` not recognised.".format(date))
-        #    return
-        date = date + " "
-            
+        date = kwargs['d'] + " "
+
     if 't' in kwargs.keys():
-        time = kwargs['t']
-        #unmatched_formats = 0
-        #for time_format in time_formats:
-        #    try:
-        #        datetime.strptime(time, time_format)
-        #        break
-        #    except ValueError:
-        #        unmatched_formats +=1
-        #if unmatched_formats == len(time_formats):
-        #    await message.channel.send("Error: Time `{}` not recognised.".format(time))
-        #    return
-        time = "at " + time
-        
-    match_message_text = "**Match scheduled:**        :one: {} vs :two: {}        **{}{}**".format(team1, team2, date, time)
-    
-    match_message = await message.channel.send(match_message_text)
+        time = "at " + kwargs['t']
+
+    timestamp = date + time
+
+    match_embed = Embed(title="**Match scheduled**", color=0xf1c40f)
+    match_embed.add_field(name=":one: " + team1.name, value=team1_players, inline=True)
+    match_embed.add_field(name=":two: " + team2.name, value=team2_players, inline=True)
+    match_embed.set_footer(text=timestamp)
+
+    match_message = await message.channel.send(embed=match_embed)
     await match_message.add_reaction("1\u20e3")
     await match_message.add_reaction("2\u20e3")
-    
-    
+
+
+
 async def help(message, args, kwargs):
     # If you want to edit what happens when the user asks for help with
     # `!command --help`, you can edit this function
