cc8af01a219e43aaba465056577677d69ed8c691
speedstyle <jamaljamalwise@gmail.com>
2021-02-05 16:18:38 +0000

    Remove role member finder (thanks to `Role.members`)

--- modules/utility_functions.py
+++ modules/utility_functions.py
@@ -6,17 +6,6 @@ def run(bot):
     bot.run(bot_token)
 
 ###############################################################################
-
-def get_users_with_role(ctx, role_id):
-    role_id = int(role_id)
-    users = []
-    for user in ctx.guild.members:
-        user_roles_ids = [r.id for r in user.roles]
-        if role_id in user_roles_ids:
-            users.append(user)
-    return users
-
-###############################################################################
 from os import scandir
 def clear_data():
     for file in scandir(r"data"):
