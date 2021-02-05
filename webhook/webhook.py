236dc907bf781a308f288b62c298ae844d307d67
theo-brown <7982453+theo-brown@users.noreply.github.com>
2021-02-05 21:19:10 +0000

    Use Flask's request.get_json() function

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/tournabot-webhook', methods=['POST'])
 def respond():
-    payload = request.json
-    if payload['ref'] == "refs/head/main":
+    payload = request.get_json()
+    print(payload)
+    if payload["ref"] == "refs/head/main":
         print("Pulling repo....")
         subprocess.run("./bot_update.sh", shell=True)
     return Response(status=200)
