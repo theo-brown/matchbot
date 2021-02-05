cb8b833eaf848301671a67d72799e5d6531784b9
theo-brown <7982453+theo-brown@users.noreply.github.com>
2021-02-05 21:11:09 +0000

    Fix webhook to listen to pushes to refs/heads/main

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/tournabot-webhook', methods=['POST'])
 def respond():
     payload = request.json
-    if payload['ref'] == "refs/head/master":
+    if payload['ref'] == "refs/head/main":
         print("Pulling repo....")
         subprocess.run("./bot_update.sh", shell=True)
     return Response(status=200)
