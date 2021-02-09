78ef9cd339fe02ab8701c138891f46867c58ad24
theo-brown <7982453+theo-brown@users.noreply.github.com>
2021-02-09 17:53:07 +0000

    Replace webhook with Github Actions for CI

---
+++ .github/workflows/bot_update.sh
@@ -0,0 +1,8 @@
+source /home/tab53/3.8_env/bin/activate
+cd /home/tab53/programs/tournaBOT
+git checkout main
+git pull
+pipreqs .
+pip install -r requirements.txt
+systemctl --user daemon-reload
+systemctl --user restart tournabot.service
