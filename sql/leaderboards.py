9ad65775d48f22b3f0e81fd4daf407e84a3bf20c
theo-brown <7982453+theo-brown@users.noreply.github.com>
2021-02-15 09:58:14 +0000

    Change channels sql to use UPSERT; print channels using code syntax

--- sql/leaderboards.py
+++ sql/leaderboards.py
@@ -1,5 +1,4 @@
 import sqlite3
-from operator import itemgetter
 
 def create():  
     db = sqlite3.connect('tournabot.db')
@@ -21,10 +20,11 @@ def display():
     db = sqlite3.connect('tournabot.db')
     csr = db.cursor()
     csr.execute("SELECT * FROM leaderboards")
-    print("id\tname\tguild_id")
+    s = "id\tname\tguild_id"
     for entry in csr.fetchall():
-        print("\n{}\t{}\t{}".format(*entry))
+        s += "\n{}\t{}\t{}".format(*entry)
     db.close()
+    return s
 
 def add_row(leaderboard_name, guild_id):
     db = sqlite3.connect('tournabot.db')
@@ -52,22 +52,3 @@ def get_id(leaderboard_name, leaderboard_guild_id):
     leaderboard_id = csr.fetchone()[0]
     db.close()
     return leaderboard_id
-
-def get_list(leaderboard_id):
-    db = sqlite3.connect('tournabot.db')
-    csr = db.cursor()
-    csr.execute("SELECT user_id, points FROM points WHERE leaderboard_id=? \
-                ORDER BY points", (leaderboard_id,))
-    # Get the values as a list of lists [user_id, points]
-    l = [list(i) for i in csr.fetchall()]
-    # Sort in descending order by points (index 1)
-    l.sort(reverse=True, key=itemgetter(1))
-    # Rank by points, assigning same rank to ties
-    # Get a list starting at 1, of length equal to len(l)
-    ranks = list(range(1, len(l)+1))
-    for i in range(len(l)-1):
-        # If the next users points are the same as this one, set them equal
-        if l[i+1][1] == l[i][1]:
-            ranks[i+1] = ranks[i]
-    db.close()
-    return list(zip(ranks, [i[0] for i in l], [i[1] for i in l]))
