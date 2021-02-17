852980ccd192b2760412596dadc34fd0b62bc91e
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-02-17 16:04:01 +0000

    force pipreqs update

--- .github/workflows/bot_update.sh
+++ .github/workflows/bot_update.sh
@@ -2,7 +2,7 @@ source /home/tab53/3.8_env/bin/activate
 cd /home/tab53/programs/tournaBOT
 git checkout main
 git pull
-pipreqs .
+pipreqs . --force
 pip install -r requirements.txt
 systemctl --user daemon-reload
 systemctl --user restart tournabot.service
