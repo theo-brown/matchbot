55a233abc0761ad3f97ced398123fdf5bfaae81d
theo-brown <7982453+theo-brown@users.noreply.github.com>
2021-02-17 21:40:11 +0000

    Accept score-only inputs; restructure and fix minor style points in merge

--- bot.py
+++ bot.py
@@ -1,11 +1,10 @@
 import discord
-from discord import Role, User, Embed, Colour, Member, AllowedMentions
+from discord import Role, Embed, Colour, Member, AllowedMentions, utils
 from discord.ext import commands
 from typing import Union
 from sql import channels as chn
 from sql import leaderboards as ldb
 import parsedatetime as pdt
-from datetime import datetime
 
 # Enable the bot to see members roles etc
 bot_intents = discord.Intents.default()
@@ -27,7 +26,7 @@ async def on_ready():
 
 # MODERATOR COMMANDS
 
-# Channel    
+# Channel
 @bot.command()
 @commands.has_permissions(manage_channels=True)
 async def channels(ctx, mode="show", mode_arg=''):
@@ -66,7 +65,7 @@ async def channels(ctx, mode="show", mode_arg=''):
                 await ctx.send(help_str, delete_after=10)
     elif mode == "help":
         await ctx.send(help_str, delete_after=10)
-        
+
 
 @bot.listen()
 async def on_message(msg):
@@ -80,22 +79,23 @@ async def on_message(msg):
 # MATCH COMMAND
 
 @bot.command()
-async def match(ctx, team1: Union[Role, Member, str], team2: Union[Role, Member, str], schedule_args=''):
+async def match(ctx, team1: Union[Role, Member, str],
+                team2: Union[Role, Member, str], schedule_args=''):
     date = "Today"
     time = ''
     calendar = pdt.Calendar()
-    
+
     for arg in schedule_args.split():
         time_obj, result_flag = calendar.parse(arg)
-        if result_flag == 1: # If this is arg is only a date
+        if result_flag == 1: # If this arg is only a date
             date = arg
-        elif result_flag == 2: # if this arg is only a time
+        elif result_flag == 2: # If this arg is only a time
             time = " at " + arg
 
     embed = Embed(title="**Match scheduled**", colour=Colour.gold())
     embed.set_footer(text=date+time)
 
-    for i,team in enumerate((team1, team2)):
+    for i, team in enumerate((team1, team2)):
         emoji = f'{i+1}\N{COMBINING ENCLOSING KEYCAP} '
         if isinstance(team, Member):
             embed.add_field(name=emoji, value=team.mention)
@@ -115,89 +115,83 @@ async def match(ctx, team1: Union[Role, Member, str], team2: Union[Role, Member,
 @bot.command()
 async def result(ctx, *args):
     if ctx.message.reference is None:
-        await ctx.send("Error: `!result` must be sent as a reply to the match message.")
+        await ctx.send("Error: `!result` must be sent as a reply to a match message.")
         return
-    if len(args) % 2 or '-' in args[0]:
-        await ctx.send("Error: please supply an even number of arguments (MAP/GAME and SCORE)")
+    if len(args) == 0:
+        await ctx.send("Error: `!result` expects arguments: either scores only "
+                       "(e.g. `16-10 15-16 etc`), or maps/games and scores "
+                       "e.g. `Map1 16-10 etc`)")
         return
 
     match_message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
     match_embed = match_message.embeds[0]
-    team1 = match_embed.fields[0].name
-    team1_players = match_embed.fields[0].value
-    team2 = match_embed.fields[1].name
-    team2_players = match_embed.fields[1].value
+    teams = (match_embed.fields[0].name, match_embed.fields[1].name)
+    players = (match_embed.fields[0].value, match_embed.fields[1].value)
+    result_embed = Embed(title="**Match Result**", description='', colour=Colour.green())
 
-    embed = Embed(title="**Match Result**", description='', colour=Colour.green())
-
-    maps_won = [0,0]
+    games_won = [0, 0]
     round_diff = 0
 
-    iterator = iter(args)  # trick for taking pairs from args
-    for map_,score in zip(iterator, iterator):
-        t1,t2 = map(int, score.split('-'))
-        round_diff += t1 - t2
+    game = ''
+    for arg in args:
+        if '-' not in arg:
+            # Args without '-' are names of games/maps
+            game = f"{arg}: "
+        else:
+            # Args with '-' are scorelines
+            score1, score2 = map(int, arg.split('-'))
+            round_diff += score1 - score2
+            score1_str = f"**{score1}**" if score1 >= score2 else score1
+            score2_str = f"**{score2}**" if score1 <= score2 else score2
+            result_embed.description += f"{game}{score1_str} - {score2_str}"
+            game = '' # Game must be reset to avoid duplicates
 
-        t1_str = f"**{t1}**" if t1>=t2 else t1
-        t2_str = f"**{t2}**" if t1<=t2 else t2
-        embed.description += f"{map_}: {t1_str} - {t2_str}"
+            # Wins give a point to winner, draws give half a point each
+            games_won[0] += (score1 > score2)
+            games_won[1] += (score1 < score2)
+            if score1 == score2:
+                games_won[0] += 1/2
+                games_won[1] += 1/2
 
-        maps_won[0] += t1>t2
-        maps_won[1] += t1<t2
-        if t1==t2:
-            maps_won[0] += 1/2
-            maps_won[1] += 1/2
+    # Determine winner and display in embed
+    if games_won[0] > games_won[1]:
+        winner_emote = "1\N{COMBINING ENCLOSING KEYCAP}"
+        loser_emote = "2\N{COMBINING ENCLOSING KEYCAP}"
+        footertext = teams[0][3:] + " wins"
+        footericon = "https://twemoji.maxcdn.com/v/latest/72x72/31-20e3.png"
+    elif games_won[1] > games_won[0]:
+        winner_emote = "2\N{COMBINING ENCLOSING KEYCAP}"
+        loser_emote = "1\N{COMBINING ENCLOSING KEYCAP}"
+        footertext = teams[1][3:] + " wins",
+        footericon = "https://twemoji.maxcdn.com/v/latest/72x72/32-20e3.png"
+    elif games_won[0] == games_won[1]:
+        winner_emote, loser_emote = None, None
+        footertext = "Match tied",
+        footericon = "https://twemoji.maxcdn.com/v/latest/72x72/1f522.png"
 
-    emotes = {
-        1: "1\N{COMBINING ENCLOSING KEYCAP}",
-        2: "2\N{COMBINING ENCLOSING KEYCAP}",
-        None: "\N{INPUT SYMBOL FOR NUMBERS}",
-    }
+    # Round diff is (team 1 score - team 2 score)
+    result_embed.add_field(name=f"{teams[0]} ({round_diff:+})", value=players[0])
+    result_embed.add_field(name=f"{teams[1]} ({-round_diff:+})", value=players[1])
+    result_embed.set_footer(text=footertext, icon_url=footericon)
+    await match_message.reply(embed=result_embed)
 
-    if maps_won[0] > maps_won[1]:
-        embed.set_footer(
-            text=team1[3:] + " wins",
-            icon_url="https://twemoji.maxcdn.com/v/latest/72x72/31-20e3.png",
-        )
-        winner = 1
-    elif maps_won[1] > maps_won[0]:
-        embed.set_footer(
-            text=team2[3:] + " wins",
-            icon_url="https://twemoji.maxcdn.com/v/latest/72x72/32-20e3.png",
-        )
-        winner = 2
-    elif maps_won[0] == maps_won[1]:
-        embed.set_footer(
-            text="Match tied",
-            icon_url="https://twemoji.maxcdn.com/v/latest/72x72/1f522.png",
-        )
-        winner = None
+    # Award points to users that made correct predictions
+    correct, incorrect = set(), set()
+    for emote, users in zip((winner_emote, loser_emote), (correct, incorrect)):
+        reaction = utils.get(match_message.reactions, emoji=emote)
+        async for user in reaction.users():
+            users.add(user)
 
-    embed.add_field(name=f"{team1} ({round_diff:+})", value=team1_players)
-    embed.add_field(name=f"{team2} ({-round_diff:+})", value=team2_players)
-    await match_message.reply(embed=embed)
-
-    async def get_reactors(team):
-        react = utils.get(match_message.reactions, emoji=emotes[team])
-        if not react:
-            return set()  # reaction not present
-        return {user.id async for user in react.users() if not user.bot}
-
-    winners = await get_reactors(winner)
-    losers = set.union(*[await get_reactors(t) for t in emotes if t!=winner])
-    players = team1_players + team2_players
-
-    winners = {w for w in winners if w not in losers and w.mention not in players}
-
-    for user in winners:
+    players_set = {user.id for user in match_message.mentions}
+    for user in correct - set().union(incorrect, players_set):
         ldb.add_points(match_message.channel.id, user.id, 1)
 
     await match_message.reply(ldb.get_message(match_message.channel.id),
-                              allowed_mentions=AllowedMentions(users=list(winners+losers)))
+                              allowed_mentions=AllowedMentions(users=list(set().union(incorrect, correct))))
 
 ###############################################################################
 
-# LEADERBOARD COMMAND    
+# LEADERBOARD COMMAND
 @bot.command()
 async def leaderboard(ctx, *args):
     args = list(args)
