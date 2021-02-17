4f5b7691f958d7e500975c648ab845227c12a617
speedstyle <jamaljamalwise@gmail.com>
2021-02-17 16:06:30 +0000

    Rewrite !result (speedstyle PR (#3)

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-from discord import Role, User, Embed, Colour, Member
+from discord import Role, User, Embed, Colour, Member, AllowedMentions
 from discord.ext import commands
 from typing import Union
 from sql import channels as chn
@@ -115,71 +115,85 @@ async def match(ctx, team1: Union[Role, Member, str], team2: Union[Role, Member,
 @bot.command()
 async def result(ctx, *args):
     if ctx.message.reference is None:
-        await ctx.send("Error: `!result` must be sent as a reply to a match message.")
+        await ctx.send("Error: `!result` must be sent as a reply to the match message.")
         return
-    elif len(args) % 2:
+    if len(args) % 2 or '-' in args[0]:
         await ctx.send("Error: please supply an even number of arguments (MAP/GAME and SCORE)")
         return
-    else:
-        match_message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
-        match_embed = match_message.embeds[0]
-        team1 = match_embed.fields[0].name
-        team1_players = match_embed.fields[0].value
-        team2 = match_embed.fields[1].name
-        team2_players = match_embed.fields[1].value
 
-        scores = {}
-        for i in range(len(args) // 2):
-            map_i = args[2 * i]
-            score_i = args[2 * i + 1]
-            scores[map_i] = score_i
+    match_message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+    match_embed = match_message.embeds[0]
+    team1 = match_embed.fields[0].name
+    team1_players = match_embed.fields[0].value
+    team2 = match_embed.fields[1].name
+    team2_players = match_embed.fields[1].value
 
-        maps_won = [0, 0]
-        round_diff = 0
+    embed = Embed(title="**Match Result**", description='', colour=Colour.green())
 
-        for map_played, map_score in scores.items():
-            map_score_list = map_score.split('-')
-            map_score_t1 = int(map_score_list[0])
-            map_score_t2 = int(map_score_list[1])
-            round_diff += map_score_t1 - map_score_t2
-            if map_score_t1 > map_score_t2:
-                scores[map_played] = "**{}** - {}\n".format(map_score_t1, map_score_t2)
-                maps_won[0] += 1
-            else:
-                scores[map_played] = "{} - **{}**\n".format(map_score_t1, map_score_t2)
-                maps_won[1] += 1
+    maps_won = [0,0]
+    round_diff = 0
 
-        if maps_won[0] > maps_won[1]:
-            winner = team1[3:] + " wins"
-            winner_icon = "https://twemoji.maxcdn.com/v/latest/72x72/31-20e3.png"
-            winner_reaction_index = 0
-        else:
-            winner = team2[3:] + " wins"
-            winner_icon = "https://twemoji.maxcdn.com/v/latest/72x72/32-20e3.png"
-            winner_reaction_index = 1
+    iterator = iter(args)  # trick for taking pairs from args
+    for map_,score in zip(iterator, iterator):
+        t1,t2 = map(int, score.split('-'))
+        round_diff += t1 - t2
 
-        description = ""
-        for map_played, map_score in scores.items():
-            description += map_played + ": " + map_score
-        
-        # Send result embed
-        result_embed = discord.Embed(title="**Match result**", color=0x2ecc71)
-        result_embed.description = description
-        result_embed.add_field(name=team1 + " ({0:+})".format(round_diff), value=team1_players, inline=True)
-        result_embed.add_field(name=team2 + " ({0:+})".format(-round_diff), value=team2_players, inline=True)
-        result_embed.set_footer(text=winner, icon_url=winner_icon)
-        await match_message.reply(embed=result_embed)
+        t1_str = f"**{t1}**" if t1>=t2 else t1
+        t2_str = f"**{t2}**" if t1<=t2 else t2
+        embed.description += f"{map_}: {t1_str} - {t2_str}"
 
-        # Update predictions leaderboard for this channel
-        correct_reactors = await match_message.reactions[winner_reaction_index].users().flatten()
-        incorrect_reactors = await match_message.reactions[not winner_reaction_index].users().flatten()
-        correct_reactors = [reactor for reactor in correct_reactors 
-                            if reactor not in incorrect_reactors
-                            and reactor.mention not in team1_players + team2_players]
-        for user in correct_reactors:
-            ldb.add_points(match_message.channel.id, user.id, 1)
-        
-        await match_message.reply(ldb.get_message(match_message.channel.id))
+        maps_won[0] += t1>t2
+        maps_won[1] += t1<t2
+        if t1==t2:
+            maps_won[0] += 1/2
+            maps_won[1] += 1/2
+
+    emotes = {
+        1: "1\N{COMBINING ENCLOSING KEYCAP}",
+        2: "2\N{COMBINING ENCLOSING KEYCAP}",
+        None: "\N{INPUT SYMBOL FOR NUMBERS}",
+    }
+
+    if maps_won[0] > maps_won[1]:
+        embed.set_footer(
+            text=team1[3:] + " wins",
+            icon_url="https://twemoji.maxcdn.com/v/latest/72x72/31-20e3.png",
+        )
+        winner = 1
+    elif maps_won[1] > maps_won[0]:
+        embed.set_footer(
+            text=team2[3:] + " wins",
+            icon_url="https://twemoji.maxcdn.com/v/latest/72x72/32-20e3.png",
+        )
+        winner = 2
+    elif maps_won[0] == maps_won[1]:
+        embed.set_footer(
+            text="Match tied",
+            icon_url="https://twemoji.maxcdn.com/v/latest/72x72/1f522.png",
+        )
+        winner = None
+
+    embed.add_field(name=f"{team1} ({round_diff:+})", value=team1_players)
+    embed.add_field(name=f"{team2} ({-round_diff:+})", value=team2_players)
+    await match_message.reply(embed=embed)
+
+    async def get_reactors(team):
+        react = utils.get(match_message.reactions, emoji=emotes[team])
+        if not react:
+            return set()  # reaction not present
+        return {user.id async for user in react.users() if not user.bot}
+
+    winners = await get_reactors(winner)
+    losers = set.union(*[await get_reactors(t) for t in emotes if t!=winner])
+    players = team1_players + team2_players
+
+    winners = {w for w in winners if w not in losers and w.mention not in players}
+
+    for user in winners:
+        ldb.add_points(match_message.channel.id, user.id, 1)
+
+    await match_message.reply(ldb.get_message(match_message.channel.id),
+                              allowed_mentions=AllowedMentions(users=list(winners+losers)))
 
 ###############################################################################
 
