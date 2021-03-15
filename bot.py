ed0d8b46c2abd05534021686b9206e891f611060
theo-brown <7982453+theo-brown@users.noreply.github.com>
2021-03-15 10:51:44 +0000

    Add error logging to channel

--- bot.py
+++ bot.py
@@ -27,5 +27,19 @@ bot.load_extension('cogs.users')
 bot.load_extension('cogs.lobby')
 bot.load_extension('cogs.match')
 
+@bot.event
+async def on_command_error(ctx, error):
+    error_string = ('Error raised\n'
+                    f'In server: {ctx.guild.name}\n'
+                    f'In channel: {ctx.channel.name}\n'
+                    f'From user: {ctx.author}\n'
+                    f'In message: {ctx.message.content}\n'
+                    f'Error: {error}')
+
+    logging_channel = bot.get_channel(int(getenv('LOG_CHANNEL_ID')))
+    await logging_channel.send(f'```{error_string}```')
+
+    logging.error(error_string, exc_info=error)
+
 if __name__ == '__main__':
     bot.run(getenv('BOT_TOKEN'))
