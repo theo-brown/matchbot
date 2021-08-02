ac01e5fb1040b20371124df2be873488888546b4
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-02 18:26:16 +0100

    Working Postgres + aiopg docker compose app

---
+++ matchbot_app/database/init/create_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS users (steam_id     BIGINT      NOT NULL,
+                                  discord_id   BIGINT      NOT NULL,
+                                  display_name VARCHAR(64) NOT NULL,
+                                  PRIMARY KEY (steam_id),
+                                  CONSTRAINT unique_user UNIQUE (steam_id, discord_id, display_name));
+
+CREATE TABLE IF NOT EXISTS teams (team_id           VARCHAR(32) NOT NULL,
+                                  team_name         VARCHAR(64) NOT NULL,
+                                  team_tag          VARCHAR(15) NOT NULL DEFAULT '',
+                                  PRIMARY KEY (team_id));
+
+CREATE TABLE IF NOT EXISTS team_players (team_id  VARCHAR(32) NOT NULL,
+                                         steam_id BIGINT      NOT NULL,
+                                         PRIMARY KEY (team_id),
+                                         CONSTRAINT unique_player UNIQUE (team_id, steam_id));
---
+++ matchbot_app/web/run.py
@@ -0,0 +1,22 @@
+import asyncio
+import aiopg
+from os import getenv
+
+
+async def main():
+    while True:
+        try:
+            async with await aiopg.connect(user=getenv("POSTGRES_USER"),
+                                     database=getenv("POSTGRES_DB"),
+                                     password=getenv("POSTGRES_PASSWORD"),
+                                     host="database") as db:
+                async with await db.cursor() as cur:
+                    await cur.execute("SELECT * FROM information_schema.tables WHERE table_schema = 'public'")
+                    ret = await cur.fetchall()
+                    return ret
+        except:
+            continue
+
+
+if __name__ == "__main__":
+    print(asyncio.run(main()))
