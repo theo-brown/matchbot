6745dac4bd820393118883be1eef9134064ff681
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-02 18:36:08 +0100

    Fix SQL formatting

--- matchbot_app/database/init/create_tables.sql
+++ matchbot_app/database/init/create_tables.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS users (steam_id     BIGINT      NOT NULL,
-                                  discord_id   BIGINT      NOT NULL,
+                                  discord_id   BIGINT,
                                   display_name VARCHAR(64) NOT NULL,
                                   PRIMARY KEY (steam_id),
                                   CONSTRAINT unique_user UNIQUE (steam_id, discord_id, display_name));
 
-CREATE TABLE IF NOT EXISTS teams (team_id           VARCHAR(32) NOT NULL,
-                                  team_name         VARCHAR(64) NOT NULL,
-                                  team_tag          VARCHAR(15) NOT NULL DEFAULT '',
+CREATE TABLE IF NOT EXISTS teams (team_id   VARCHAR(32) NOT NULL,
+                                  team_name VARCHAR(64) NOT NULL,
+                                  team_tag  VARCHAR(15) NOT NULL DEFAULT '',
                                   PRIMARY KEY (team_id));
 
 CREATE TABLE IF NOT EXISTS team_players (team_id  VARCHAR(32) NOT NULL,
