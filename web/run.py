b1512859c7ea8980672eb71e32ce7fb04f7990d7
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-03 19:39:38 +0100

    Streamline example

--- web/run.py
+++ web/run.py
@@ -6,14 +6,13 @@ from os import getenv
 async def main():
     while True:
         try:
-            async with await aiopg.connect(user=getenv("POSTGRES_USER"),
-                                           database=getenv("POSTGRES_DB"),
-                                           password=getenv("POSTGRES_PASSWORD"),
-                                           host="database") as db:
-                async with await db.cursor() as cur:
+            async with aiopg.connect(user=getenv("POSTGRES_USER"),
+                                     database=getenv("POSTGRES_DB"),
+                                     password=getenv("POSTGRES_PASSWORD"),
+                                     host="database") as db:
+                async with db.cursor() as cur:
                     await cur.execute("SELECT * FROM information_schema.tables WHERE table_schema = 'public'")
-                    ret = await cur.fetchall()
-                    return ret
+                    return await cur.fetchall()
         except:
             continue
 
