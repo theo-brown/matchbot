15b106bc50e5c9274283f02f867edde03603e196
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-03 19:38:20 +0100

    Assign matches to servers rather than dynamically creating servers

--- src/matchbot/gameservermanager/__init__.py
+++ src/matchbot/gameservermanager/__init__.py
@@ -1,48 +1,32 @@
 import aiodocker
 from matchbot import Match
 from matchbot.gameservermanager.gameserver import GameServer
+from typing import Iterable, Optional
 
 
 class GameServerManager:
-    def __init__(self, server_token: str, server_ip: str, server_port_min: int, server_port_max: int):
+    def __init__(self, servers: Iterable[GameServer]):
         self.docker = aiodocker.Docker()
-        self.current_matches = {}
+        self.servers = list(servers)
 
-        self.server_token = server_token
-        self.ip = server_ip
-
-        port_range = [i for i in range(int(server_port_min), int(server_port_max) + 1)]
-        self.max_number_of_servers = len(port_range) // 2
-        connect_ports = port_range[:self.max_number_of_servers]
-        gotv_ports = port_range[self.max_number_of_servers:]
-        self.ports = [{"port": i, "gotv_port": j} for i, j in zip(connect_ports, gotv_ports)]
-        self.available_ports = set(self.ports)
+    def get_available_server(self) -> Optional[GameServer]:
+        for server in self.servers:
+            if not server.is_assigned:
+                return server
 
     async def start_match(self, match: Match):
-        if len(self.available_ports) == 0:
-            raise IndexError(f"{self.max_number_of_servers} exceeded, cannot host Match {match.id}")
+        if server := self.get_available_server():
+            server.match = match
+            await server.start(docker_instance=self.docker)
+        else:
+            raise ValueError("Not enough available servers.")
 
-        self.current_matches[match.id] = match
-
-        server_ports = self.available_ports.pop()
-        match.server = GameServer(token=self.server_token,
-                                  ip=self.ip,
-                                  port=server_ports["port"],
-                                  gotv_port=server_ports["gotv_port"],
-                                  id=match.id)
-
-        await match.server.start(docker_instance=self.docker)
-
-    def get_match(self, match_id: str) -> Match:
-        return self.current_matches[match_id]
+    def get_server(self, match_id: str) -> GameServer:
+        for server in self.servers:
+            if server.match.id == match_id:
+                return server
 
     async def delete_match(self, match_id):
-        match = self.get_match(match_id)
-        if match.server is not None:
-            port = match.server.port
-            gotv_port = match.server.gotv_port
-            await match.server.stop()
-            del match.server
-            match.server = None
-            self.available_ports.add({"port": port, "gotv_port": gotv_port})
-        self.current_matches.pop(match_id)
+        server = self.get_server(match_id)
+        await server.stop()
+        server.match = None
--- src/matchbot/gameservermanager/gameserver.py
+++ src/matchbot/gameservermanager/gameserver.py
@@ -4,27 +4,30 @@ import asyncio
 import aiorcon
 import aiodocker
 from time import time
+from matchbot import Match
 
 
 class GameServer:
-    def __init__(self, token, ip, port=27015, gotv_port=27020, id=uuid4().hex, match_config=""):
+    def __init__(self, token, ip, port=27015, gotv_port=27020, id=uuid4().hex):
         self.token = token
         self.id = id
         self.ip = ip
         self.port = port
         self.gotv_port = gotv_port
+        self.container = None
+        self.rcon = None
+        self.match = None
+
+    async def start(self, docker_instance: aiodocker.docker.Docker, timeout=8):
+        if self.match is None:
+            raise ValueError("No match loaded.")
+
         self.password = token_urlsafe(6)
         self.rcon_password = token_urlsafe(6)
         self.gotv_password = token_urlsafe(6)
-        self.match_config = match_config
-
         self.connect_str = f"connect {self.ip}:{self.port}; password {self.password}"
         self.connect_gotv_str = f"connect {self.ip}:{self.gotv_port}; password {self.gotv_password}"
 
-        self.container = None
-        self.rcon = None
-
-    async def start(self, docker_instance: aiodocker.docker.Docker, timeout=8):
         config = {"Image": "theobrown/csgo-docker:latest",
                   "Env": [f"SERVER_TOKEN={self.token}",
                           f"PORT={self.port}",
@@ -32,7 +35,7 @@ class GameServer:
                           f"PASSWORD={self.password}",
                           f"RCON_PASSWORD={self.rcon_password}",
                           f"GOTV_PASSWORD={self.gotv_password}",
-                          f"MATCH_CONFIG={self.match_config}"],
+                          f"MATCH_CONFIG={self.match.config}"],
                   "HostConfig": {"NetworkMode": "host"}}
         print("Spawning server...")
         self.container = await docker_instance.containers.run(config=config, name=self.id)
@@ -40,7 +43,7 @@ class GameServer:
         print("Establishing RCON connection...")
         start_time = time()
         duration = 0
-        while (duration < timeout):
+        while duration < timeout:
             duration = time() - start_time
             try:
                 self.rcon = await aiorcon.RCON.create(self.ip, self.port, self.rcon_password,
@@ -67,3 +70,7 @@ class GameServer:
             await self.container.delete(force=True)
         self.rcon = None
         self.container = None
+
+    @property
+    def is_assigned(self) -> bool:
+        return bool(self.match)
