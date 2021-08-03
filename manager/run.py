5ec78ebca53e7e9bf8871e20721b53c7598d2909
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-03 23:06:51 +0100

    Use DatabaseInterface for Manager's connection to the db

--- manager/run.py
+++ manager/run.py
@@ -1,9 +1,24 @@
-from matchbot.gameservermanager import GameServerManager
+from matchbot.gameserver import GameServer, GameServerManager
+from matchbot.database import DatabaseInterface
+
 from os import getenv
+import asyncio
 
-gsm = GameServerManager(server_token=getenv("SERVER_TOKEN"),
-                        server_ip=getenv("SERVER_IP"),
-                        server_port_min=getenv("SERVER_PORT_MIN"),
-                        server_port_max=getenv("SERVER_PORT_MAX"))
+dbi = DatabaseInterface(host="database",
+                        user=getenv("POSTGRES_USER"),
+                        password=getenv("POSTGRES_PASSWORD"),
+                        database_name=getenv("POSTGRES_DB"))
 
-print("Hello from manager")
+async def main():
+    gameservers = await dbi.generate_gameservers()
+    print(gameservers)
+    await dbi.add_gameserver("asdasd", "192.168.0.1", 27015, 27020)
+    gameservers = await dbi.generate_gameservers()
+    print(gameservers)
+
+if __name__ == "__main__":
+    try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+    finally:
+        dbi.close()
