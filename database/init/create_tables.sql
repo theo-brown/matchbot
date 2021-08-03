facb3a3608769a159b207113908cd7d8f394a9d5
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-03 23:06:08 +0100

    Add sql table for game servers

--- database/init/create_tables.sql
+++ database/init/create_tables.sql
@@ -13,3 +13,9 @@ CREATE TABLE IF NOT EXISTS team_players (team_id  VARCHAR(32) NOT NULL,
                                          steam_id BIGINT      NOT NULL,
                                          PRIMARY KEY (team_id),
                                          CONSTRAINT unique_player UNIQUE (team_id, steam_id));
+
+CREATE TABLE IF NOT EXISTS game_servers (server_token VARCHAR(32) NOT NULL,
+                                         ip           CIDR        NOT NULL,
+                                         port         INTEGER     NOT NULL,
+                                         gotv_port    INTEGER     NOT NULL,
+                                         PRIMARY KEY (server_token));
