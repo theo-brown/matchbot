f0833aeee80202501fd4870f703774a388be75ce
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-11 18:15:50 +0100

    Separate table creation into separate file

--- database/init/1_create_tables.sql
+++ database/init/1_create_tables.sql
@@ -2,34 +2,14 @@ CREATE TABLE IF NOT EXISTS maps (id   VARCHAR(32) NOT NULL,
                                  name VARCHAR(32) NOT NULL,
                                  PRIMARY KEY (id));
 
-INSERT INTO maps(id, name) VALUES ('de_ancient', 'Ancient'),
-                                  ('de_calavera', 'Calavera'),
-                                  ('de_cbble', 'Cobblestone'),
-                                  ('de_dust2', 'Dust 2'),
-                                  ('de_inferno', 'Inferno'),
-                                  ('de_mirage', 'Mirage'),
-                                  ('de_nuke', 'Nuke'),
-                                  ('de_overpass', 'Overpass'),
-                                  ('de_pitstop', 'Pitstop'),
-                                  ('de_shortdust', 'Shortdust'),
-                                  ('de_shortnuke', 'Nuke'),
-                                  ('de_train', 'Train'),
-                                  ('de_vertigo', 'Vertigo');
-
 CREATE TABLE IF NOT EXISTS sides (side VARCHAR(32) NOT NULL,
                                   PRIMARY KEY (side));
-
-INSERT INTO sides(side) VALUES ('knife'),
-                               ('team1_ct'),
-                               ('team1_t'),
-                               ('team2_ct'),
-                               ('team2_t');
 
 CREATE TABLE IF NOT EXISTS server_tokens (token VARCHAR(32) NOT NULL,
                                           PRIMARY KEY (token));
 
 CREATE TABLE IF NOT EXISTS users (steam_id     BIGINT      NOT NULL,
-                                  discord_id   BIGINT,
+                                  discord_id   BIGINT               DEFAULT NULL,
                                   display_name VARCHAR(64) NOT NULL,
                                   PRIMARY KEY (steam_id),
                                   CONSTRAINT unique_user UNIQUE (steam_id, discord_id, display_name));
@@ -43,11 +23,13 @@ CREATE TABLE IF NOT EXISTS team_players (team_id  VARCHAR(32) NOT NULL REFERENCE
                                          steam_id BIGINT      NOT NULL REFERENCES users(steam_id),
                                          PRIMARY KEY (team_id, steam_id));
 
-CREATE TABLE IF NOT EXISTS matches (id        VARCHAR(32) NOT NULL,
-                                    status    INTEGER     NOT NULL DEFAULT 0,
-                                    live_timestamp TIMESTAMP   NOT NULL,
-                                    team1_id  VARCHAR(32) NOT NULL REFERENCES teams(id),
-                                    team2_id  VARCHAR(32) NOT NULL REFERENCES teams(id),
+CREATE TABLE IF NOT EXISTS matches (id                 VARCHAR(32) NOT NULL,
+                                    status             VARCHAR(32) NOT NULL DEFAULT 'CREATED',
+                                    created_timestamp  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                    live_timestamp     TIMESTAMP            DEFAULT NULL,
+                                    finished_timestamp TIMESTAMP            DEFAULT NULL,
+                                    team1_id           VARCHAR(32) NOT NULL REFERENCES teams(id),
+                                    team2_id           VARCHAR(32) NOT NULL REFERENCES teams(id),
                                     PRIMARY KEY (id));
 
 CREATE TABLE IF NOT EXISTS match_maps (match_id   VARCHAR(32) NOT NULL REFERENCES matches(id),
@@ -56,8 +38,8 @@ CREATE TABLE IF NOT EXISTS match_maps (match_id   VARCHAR(32) NOT NULL REFERENCE
                                        side       VARCHAR(32) NOT NULL REFERENCES sides(side),
                                        PRIMARY KEY (match_id, map_number));
 
-CREATE TABLE IF NOT EXISTS servers (id            VARCHAR(32) NOT NULL UNIQUE,
-                                    token         VARCHAR(32) NOT NULL UNIQUE,
+CREATE TABLE IF NOT EXISTS servers (id            VARCHAR(32) NOT NULL,
+                                    token         VARCHAR(32) NOT NULL UNIQUE REFERENCES server_tokens(token),
                                     ip            INET,
                                     port          INTEGER     UNIQUE,
                                     gotv_port     INTEGER     UNIQUE,
---
+++ database/init/2_populate_defaults.sql
@@ -0,0 +1,19 @@
+INSERT INTO sides(side) VALUES ('knife'),
+                               ('team1_ct'),
+                               ('team1_t'),
+                               ('team2_ct'),
+                               ('team2_t');
+
+INSERT INTO maps(id, name) VALUES ('de_ancient', 'Ancient'),
+                                  ('de_calavera', 'Calavera'),
+                                  ('de_cbble', 'Cobblestone'),
+                                  ('de_dust2', 'Dust 2'),
+                                  ('de_inferno', 'Inferno'),
+                                  ('de_mirage', 'Mirage'),
+                                  ('de_nuke', 'Nuke'),
+                                  ('de_overpass', 'Overpass'),
+                                  ('de_pitstop', 'Pitstop'),
+                                  ('de_shortdust', 'Shortdust'),
+                                  ('de_shortnuke', 'Nuke'),
+                                  ('de_train', 'Train'),
+                                  ('de_vertigo', 'Vertigo');
