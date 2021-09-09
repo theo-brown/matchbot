05f572cf75cc747888c9f91211351dfe2dd6a153
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-09-09 23:24:18 +0100

    Update demo to include match table

--- src/matchbot/apps/demo.py
+++ src/matchbot/apps/demo.py
@@ -2,14 +2,13 @@ import asyncio
 from os import getenv
 from dotenv import load_dotenv
 from matchbot import redis
-from matchbot.database import new_pool, gameserver, user, team
+from matchbot.database import new_pool, gameserver, user, team, match
 from random import randint
 
 load_dotenv("../../../.env")
 
 
 async def main():
-
     pool = await new_pool(host="localhost",
                           port=getenv("POSTGRES_PORT"),
                           user=getenv("POSTGRES_USER"),
@@ -19,27 +18,39 @@ async def main():
     gameservers_table = gameserver.GameServersTable(pool)
     users_table = user.UsersTable(pool)
     teams_table = team.TeamsTable(pool)
+    matches_table = match.MatchesTable(pool)
 
-    def fake_steamid():
-        return randint(1e17, 1e18)
-
-    users =  [user.User(fake_steamid(), "gla1ve"),
-              user.User(fake_steamid(), "Magisk"),
-              user.User(fake_steamid(), "dev1ce"),
-              user.User(fake_steamid(), "dupreeh"),
-              user.User(fake_steamid(), "Xyp9x"),
-              user.User(fake_steamid(), "s1mple"),
-              user.User(fake_steamid(), "Boombl4"),
-              user.User(fake_steamid(), "electronic"),
-              user.User(fake_steamid(), "Perfecto"),
-              user.User(fake_steamid(), "flamie")]
+    users =  [user.User(412041897672347230, "gla1ve"),
+              user.User(989585413720717397, "Magisk"),
+              user.User(560203868182492203, "dev1ce"),
+              user.User(935591857927709199, "dupreeh"),
+              user.User(397595632166082027, "Xyp9x"),
+              user.User(141770607075093884, "s1mple"),
+              user.User(990923677509346665, "Boombl4"),
+              user.User(641151321829893629, "electronic"),
+              user.User(846354701750384649, "Perfecto"),
+              user.User(926869577381900030, "flamie")]
 
     team1 = team.Team(name="Astralis",
                       steam_ids=[user.steam_id for user in users[:5]],
-                      tag="AST")
+                      tag="AST",
+                      id="805718beaa0143af8414e7bc1df9c7ec")
 
     team2 = team.Team(name="Natus Vincere",
                       steam_ids=[user.steam_id for user in users[5:]],
-                      tag="NAVI")
+                      tag="NAVI",
+                      id="532ebcffe3264a3ea9d8a5f8a83b5c65")
+
+
+    m1 = Match(team1_id="805718beaa0143af8414e7bc1df9c7ec",
+               team2_id="532ebcffe3264a3ea9d8a5f8a83b5c65",
+               maps=["de_dust2", "de_overpass", "de_vertigo"],
+               sides=["team2_ct", "team1_ct", "knife"])
 
     await users_table.upsert(*users)
+    await teams_table.upsert(team1, team2)
+    await matches_table.upsert(m1)
+    await gameservertokens_table.insert("FIRSTTOKEN")
+    await gameservers_table.upsert(gameserver.GameServer("FIRSTTOKEN",
+                                                         "192.168.0.1",
+                                                         1, 2))
