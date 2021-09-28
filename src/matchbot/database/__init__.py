42b040b2db7e222877f5876bfa9f9c71d90ad1a8
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-09-28 11:35:24 +0100

    Use sqlalchemy for database ORM

--- src/matchbot/database/__init__.py
+++ src/matchbot/database/__init__.py
@@ -1,24 +1,106 @@
-import asyncpg
-from time import time
-import matchbot.database.match, matchbot.database.gameserver, matchbot.database.team, matchbot.database.user
+from __future__ import annotations
+from sqlalchemy import Column, Integer, BigInteger, String, ForeignKey, DateTime
+from sqlalchemy.dialects.postgresql import UUID, INET, ENUM
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.ext.associationproxy import association_proxy
 
 
-async def new_pool(host: str, port: int, user: str, password: str, database_name: str, timeout=5) -> asyncpg.pool.Pool:
-    start_time = time()
-    pool = None
+Base = declarative_base()
 
-    while pool is None and time() - start_time < timeout:
-        try:
-            pool = await asyncpg.create_pool(host=host,
-                                             port=port,
-                                             user=user,
-                                             password=password,
-                                             database=database_name,
-                                             timeout=timeout)
-        except ConnectionRefusedError:
-            continue
+MapSide = ENUM('team1_ct', 'team2_ct', 'team1_t', 'team2_t', 'knife', name='map_side')
+MatchStatus = ENUM('CREATED', 'QUEUED', 'LIVE', 'FINISHED', name='match_status')
 
-    if pool is None:
-        raise ConnectionError(f"Connection to database failed after {timeout}s")
-    else:
-        return pool
+
+class Map(Base):
+    __tablename__ = 'maps'
+
+    id = Column(String(32), primary_key=True)
+    name = Column(String(32))
+
+
+class MatchMap(Base):
+    __tablename__ = 'match_maps'
+
+    match_id = Column(UUID(as_uuid=True), ForeignKey('matches.id'), primary_key=True)
+    map_number = Column(Integer, primary_key=True)
+    map_id = Column(String(32), ForeignKey('maps.id'))
+    side = Column(MapSide)
+
+    match = relationship('Match', back_populates='maps')
+
+
+class Match(Base):
+    __tablename__ = 'matches'
+
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    status = Column(MatchStatus)
+    created_timestamp = Column(DateTime, nullable=True)
+    live_timestamp = Column(DateTime, nullable=True)
+    finished_timestamp = Column(DateTime, nullable=True)
+    team1_id = Column(UUID(as_uuid=True), ForeignKey('teams.id'))
+    team2_id = Column(UUID(as_uuid=True), ForeignKey('teams.id'))
+
+    server = relationship('Server', back_populates='match')
+    maps = relationship('MatchMap', back_populates='match')
+
+
+class ServerToken(Base):
+    __tablename__ = 'server_tokens'
+
+    token = Column(String(32), primary_key=True)
+
+
+class Server(Base):
+    __tablename__ = 'servers'
+
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    token = Column(String(32), ForeignKey('server_tokens.token'))
+    ip = Column(INET)
+    port = Column(Integer)
+    gotv_port = Column(Integer)
+    password = Column(String(32), nullable=True)
+    gotv_password = Column(String(32), nullable=True)
+    rcon_password = Column(String(32), nullable=True)
+    match_id = Column(UUID(as_uuid=True), ForeignKey('matches.id'))
+
+    match = relationship('Match', back_populates='server')
+
+
+class TeamMembership(Base):
+    __tablename__ = 'team_members'
+
+    team_id = Column(UUID(as_uuid=True), ForeignKey('teams.id'), primary_key=True)
+    steam_id = Column(BigInteger, ForeignKey('users.steam_id'), primary_key=True)
+
+    team = relationship('Team', back_populates='_membership')
+    user = relationship('User', back_populates='_membership')
+
+    def __init__(self, team: Team, user: User):
+        self.team_id = team.id
+        self.steam_id = user.steam_id
+        self.team = team
+        self.user = user
+
+
+class Team(Base):
+    __tablename__ = 'teams'
+
+    id = Column(UUID(as_uuid=True), primary_key=True)
+    name = Column(String(64))
+    tag = Column(String(15), nullable=True)
+
+    _membership = relationship('TeamMembership', back_populates='team')
+
+    users = association_proxy('_membership', 'user')
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    steam_id = Column(BigInteger, primary_key=True)
+    discord_id = Column(BigInteger, nullable=True)
+    display_name = Column(String(64))
+
+    _membership = relationship('TeamMembership', back_populates='user')
+    teams = association_proxy('_membership', 'team')
+    
