2db05804ae8b9bdb4dd03d8aeda7d583135c868e
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-10-08 16:00:43 +0100

    Create different models for create and delete

--- src/matchbot/api/models.py
+++ src/matchbot/api/models.py
@@ -1,8 +1,26 @@
 from pydantic import BaseModel
 from typing import Optional, List
+from uuid import UUID
 
 
-class User(BaseModel):
+class CreateUser(BaseModel):
     steam_id: int
-    discord_id: Optional[int] = None
     display_name: str
+    discord_id: Optional[int] = None
+
+
+class UpdateUser(BaseModel):
+    display_name: Optional[str] = None
+    discord_id: Optional[int] = None
+
+
+class CreateTeam(BaseModel):
+    id: UUID
+    name: str
+    tag: str
+    user_ids: List[int]
+
+
+class UpdateTeam(BaseModel):
+    name: Optional[str] = None
+    tag: Optional[str] = None
