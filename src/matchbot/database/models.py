7c2cb5bfa9c7edd409d5a7366c54999489fbb30a
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-10-08 15:44:07 +0100

    Use UUID type hint

--- src/matchbot/database/models.py
+++ src/matchbot/database/models.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 from sqlalchemy import Column, Integer, BigInteger, String, ForeignKey, DateTime, select
-from sqlalchemy.dialects.postgresql import UUID, INET, ENUM
+from sqlalchemy.dialects.postgresql import INET as POSTGRES_INET
+from sqlalchemy.dialects.postgresql import ENUM as POSTGRES_ENUM
+from sqlalchemy.dialects.postgresql import UUID as POSTGRES_UUID
 from sqlalchemy.orm import declarative_base, relationship
 from typing import Optional
-
+from uuid import UUID
 
 Base = declarative_base()
 
-MapSide = ENUM('team1_ct', 'team2_ct', 'team1_t', 'team2_t', 'knife', name='map_side')
-MatchStatus = ENUM('CREATED', 'QUEUED', 'LIVE', 'FINISHED', name='match_status')
+MapSide = POSTGRES_ENUM('team1_ct', 'team2_ct', 'team1_t', 'team2_t', 'knife', name='map_side')
+MatchStatus = POSTGRES_ENUM('CREATED', 'QUEUED', 'LIVE', 'FINISHED', name='match_status')
 
 
 class Map(Base):
@@ -26,7 +28,7 @@ class Map(Base):
 class MatchMap(Base):
     __tablename__ = 'match_maps'
 
-    match_id = Column(UUID(as_uuid=True), ForeignKey('matches.id'), primary_key=True)
+    match_id = Column(POSTGRES_UUID(as_uuid=True), ForeignKey('matches.id'), primary_key=True)
     map_number = Column(Integer, primary_key=True)
     map_id = Column(String(32), ForeignKey('maps.id'))
     side = Column(MapSide)
@@ -35,13 +37,13 @@ class MatchMap(Base):
 class Match(Base):
     __tablename__ = 'matches'
 
-    id = Column(UUID(as_uuid=True), primary_key=True)
+    id = Column(POSTGRES_UUID(as_uuid=True), primary_key=True)
     status = Column(MatchStatus)
     created_timestamp = Column(DateTime, nullable=True)
     live_timestamp = Column(DateTime, nullable=True)
     finished_timestamp = Column(DateTime, nullable=True)
-    team1_id = Column(UUID(as_uuid=True), ForeignKey('teams.id'))
-    team2_id = Column(UUID(as_uuid=True), ForeignKey('teams.id'))
+    team1_id = Column(POSTGRES_UUID(as_uuid=True), ForeignKey('teams.id'))
+    team2_id = Column(POSTGRES_UUID(as_uuid=True), ForeignKey('teams.id'))
 
 
 class ServerToken(Base):
@@ -53,47 +55,47 @@ class ServerToken(Base):
 class Server(Base):
     __tablename__ = 'servers'
 
-    id = Column(UUID(as_uuid=True), primary_key=True)
+    id = Column(POSTGRES_UUID(as_uuid=True), primary_key=True)
     token = Column(String(32), ForeignKey('server_tokens.token'))
-    ip = Column(INET)
+    ip = Column(POSTGRES_INET)
     port = Column(Integer)
     gotv_port = Column(Integer)
     password = Column(String(32), nullable=True)
     gotv_password = Column(String(32), nullable=True)
     rcon_password = Column(String(32), nullable=True)
-    match_id = Column(UUID(as_uuid=True), ForeignKey('matches.id'))
+    match_id = Column(POSTGRES_UUID(as_uuid=True), ForeignKey('matches.id'))
 
 
 class TeamMembership(Base):
     __tablename__ = 'team_members'
 
-    team_id = Column(UUID(as_uuid=True), ForeignKey('teams.id'), primary_key=True)
+    team_id = Column(POSTGRES_UUID(as_uuid=True), ForeignKey('teams.id'), primary_key=True)
     steam_id = Column(BigInteger, ForeignKey('users.steam_id'), primary_key=True)
 
-    def __init__(self, team: Team, user: User):
-        self.team_id = team.id
-        self.steam_id = user.steam_id
+    def __init__(self, team_id, steam_id):
+        self.team_id = team_id
+        self.steam_id = steam_id
 
 
 class Team(Base):
     __tablename__ = 'teams'
 
-    id = Column(UUID(as_uuid=True), primary_key=True)
+    id = Column(POSTGRES_UUID(as_uuid=True), primary_key=True)
     name = Column(String(64))
     tag = Column(String(15), nullable=True)
 
     users = relationship('User', secondary='team_members', back_populates='teams', lazy='selectin')
 
-    def __init__(self, id: str, name: str, tag: str):
+    def __init__(self, id: UUID, name: str, tag: str):
         self.id = id
         self.name = name
         self.tag = tag
 
+    @property
     def json(self):
         return {'id': self.id,
                 'name': self.name,
-                'tag': self.tag,
-                'users': [{user.json for user in self.users}]}
+                'tag': self.tag}
 
 
 class User(Base):
