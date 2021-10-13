dbd484c35dcbcb6995d013281b38e4fe390d4dbe
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-10-14 00:34:36 +0100

    Add argument to disable sqlalchemy echo Add print statements to event handler

--- src/matchbot/apps/__init__.py
+++ src/matchbot/apps/__init__.py
@@ -6,7 +6,8 @@ from typing import Coroutine
 class MatchbotBaseApp:
     def __init__(self,
                  db_host: str, db_port: int, db_user: str, db_password: str, db_name: str,
-                 redis_host: str, redis_port: int):
+                 redis_host: str, redis_port: int,
+                 db_echo: bool = True):
         self._db_host = db_host
         self._db_port = db_port
         self._db_user = db_user
@@ -14,21 +15,25 @@ class MatchbotBaseApp:
         self._db_name = db_name
         self._redis_host = redis_host
         self._redis_port = redis_port
+        self.db_echo = db_echo
 
         self.event_handlers = {}
 
         self.engine = db.new_engine(self._db_host, self._db_port,
-                                    self._db_user, self._db_password, self._db_name)
+                                    self._db_user, self._db_password, self._db_name,
+                                    echo=self.db_echo)
 
     def new_session(self) -> db.functions.AsyncSession:
         return db.new_session(self.engine)
 
     def new_event_handler(self, channel: str, callback: Coroutine):
+        print(f"Adding event handler on {channel}")
         self.event_handlers[channel] = EventHandler(channel, callback, host=self._redis_host, port=self._redis_port)
-        self.start_event_handler(channel)
 
     def start_event_handler(self, channel):
+        print(f"Starting event handler on {channel}")
         self.event_handlers[channel].start()
 
     def stop_event_handler(self, channel):
+        print(f"Stopping event handler on {channel}")
         self.event_handlers[channel].stop()
--- src/matchbot/database/functions.py
+++ src/matchbot/database/functions.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine
 
 
-def new_engine(host: str, port: int, user: str, password: str, db_name: str) -> AsyncEngine:
+def new_engine(host: str, port: int, user: str, password: str, db_name: str, echo: bool = True) -> AsyncEngine:
     return create_async_engine(f'postgresql+asyncpg://{user}:{password}@{host}:{port}/{db_name}',
-                               echo=True,  future=True)
+                               echo=echo,  future=True)
 
 
 def new_session(engine: AsyncEngine) -> AsyncSession:
